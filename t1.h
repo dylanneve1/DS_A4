cb10e537c42369305545dcaa2b26936edd580bb0
Dylan Neve <dylanneve1@gmail.com>
2024-11-19 19:43:57 +0000

    File dump :)

---
+++ t1.h
@@ -0,0 +1,16 @@
+#ifndef T1_H_
+#define T1_H_
+
+
+
+typedef struct Graph{
+    //TODO
+} Graph;
+
+Graph* create_graph(int num_nodes); // creates a graph with num_nodes nodes, assuming nodes are stored in alphabetical order (A, B, C..)
+void add_edge(Graph *g, int from, int to); // adds a directed edge
+void bfs(Graph* g, int origin); //implements breath first search and prints the results
+void dfs(Graph* g, int origin); //implements depth first search and prints the results
+
+
+#endif
